c3fdbc540e56bab70157f2aed09660a66509a1a4
athurman <athurman215@gmail.com>
2015-06-27 08:49:02 -0500

    add initial page templates directory

---
+++ page-templates/page-frontpage.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Front Page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="content">
+  <!-- HERO SECTION -->
+  <div id="homepage-hero">
+  </div>
+</div>
+<?php get_footer(); ?>
