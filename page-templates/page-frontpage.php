d02618e810246e90722b5510213b01da8389f597
athurman <athurman215@gmail.com>
2015-06-29 22:02:38 -0500

    add mission statement and blue buttons

--- page-templates/page-frontpage.php
+++ page-templates/page-frontpage.php
@@ -11,6 +11,7 @@ Template Name: Front Page
     <div class="wrap">
       <h1 class="text-center main">Achieve Your Health and Wellness Goals</h1>
       <h4 class="text-center subheader">Merge personal training with health and nutrition coaching to help you get there.</h4>
+      <a href="#" class="blue-btn">Get Started Today</a>
     </div>
   </div>
   <div id="what-we-offer" class="textured">
@@ -34,6 +35,19 @@ Template Name: Front Page
         <p class="text-center">Receive at home personal training with a customized workout plan to achieve your personal goals.</p>
       </div>
       <div class="clear"></div>
+    </div>
+  </div>
+  <div id="testimonials-container">
+    <div class="wrap">
+      <h3 class="text-center">"I have been a client of BJ Toups for over 7 years. He is a true professional in the field of neuromuscular therapy and fitness training. Those combined abilities and his degree in exercise physiology make him the complete package. At age 69, I am able to increase the amount of weights I lift and improve my exercise tolerance. He can massage and stretch any sore muscle and this enables me to work out on a regular basis. I credit my increased performance to BJ and whole heartedly endorse him."</h3>
+      <h4 class="text-center">Michael Levine, M.D.</h4>
+      <h6 class="text-center">Atlanta, Ga.</h6>
+    </div>
+  </div>
+  <div id="mission-statement" class="textured">
+    <div class="wrap">
+      <h2 class="text-center">Learn how to integrate health, fitness and nutrition for optimal balance and health in your life.</h2>
+      <a href="#" class="blue-btn">What We Offer</a>
     </div>
   </div>
 </div>
