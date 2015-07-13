02cf605c5a99183763f29a3e559aca8dc3f19e82
athurman <athurman215@gmail.com>
2015-07-12 22:06:47 -0500

    convert testimonial to CPT

--- page-templates/page-frontpage.php
+++ page-templates/page-frontpage.php
@@ -2,6 +2,15 @@
 /*
 Template Name: Front Page
 */
+
+$main_header = get_field('main_header');
+$sub_header = get_field('main_sub_header');
+
+$args = array(
+  'post_type'      => 'testimonial',
+  'posts_per_page' => -1
+);
+$testimonial_query = new WP_Query( $args );
 ?>
 
 <?php get_header(); ?>
@@ -9,8 +18,8 @@ Template Name: Front Page
   <!-- HERO SECTION -->
   <div id="homepage-hero">
     <div class="wrap">
-      <h1 class="text-center main">Achieve Your Health and Wellness Goals</h1>
-      <h4 class="text-center subheader">Merge personal training with health and nutrition coaching to help you get there.</h4>
+      <h1 class="text-center main"><?php echo $main_header; ?></h1>
+      <h4 class="text-center subheader"><?php echo $sub_header; ?></h4>
       <a href="#" class="blue-btn">Get Started Today</a>
     </div>
   </div>
@@ -19,19 +28,19 @@ Template Name: Front Page
       <div class="m-all t-1of3 d-1of3 icon-main">
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/mergehealth-icon-health.png" alt="Health Icon Holding Plant">
         <h2 class="text-center upcase"><span class="number">1</span></h2>
-        <h4 class="text-center upcase">Health</h4>
+        <h4 class="text-center upcase"><a href="#">Health</a></h4>
         <p class="text-center">Gain a deeper self understanding and uncover ways to move forward in positive, healthy ways.</p>
       </div>
       <div class="m-all t-1of3 d-1of3 icon-main">
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/mergehealth-icon-nutrition.png" alt="Nutrition Icon Grains">
         <h2 class="text-center upcase"><span class="number">2</span></h2>
-        <h4 class="text-center upcase">Nutrition</h4>
+        <h4 class="text-center upcase"><a href="#">Nutrition</a></h4>
         <p class="text-center">Discover how food and lifestyle choices affect every aspect of your life, including energy and stress.</p>
       </div>
       <div class="m-all t-1of3 d-1of3 icon-main">
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/mergehealth-icon-fitness.png" alt="Fitness Icon Yoga Pose">
         <h2 class="text-center upcase"><span class="number">3</span></h2>
-        <h4 class="text-center upcase">Fitness</h4>
+        <h4 class="text-center upcase"><a href="#">Fitness</a></h4>
         <p class="text-center">Receive at home personal training with a customized workout plan to achieve your personal goals.</p>
       </div>
       <div class="clear"></div>
@@ -39,9 +48,31 @@ Template Name: Front Page
   </div>
   <div id="testimonials-container">
     <div class="wrap">
-      <h3 class="text-center">"I have been a client of BJ Toups for over 7 years. He is a true professional in the field of neuromuscular therapy and fitness training. Those combined abilities and his degree in exercise physiology make him the complete package. At age 69, I am able to increase the amount of weights I lift and improve my exercise tolerance. He can massage and stretch any sore muscle and this enables me to work out on a regular basis. I credit my increased performance to BJ and whole heartedly endorse him."</h3>
-      <h4 class="text-center">Michael Levine, M.D.</h4>
-      <h6 class="text-center">Atlanta, Ga.</h6>
+      <?php
+      if ( $testimonial_query->have_posts() ) {
+        $i = 1;
+        while ( $testimonial_query->have_posts() ) {
+          $testimonial_query->the_post();
+          $new_content = strip_tags(get_the_content());
+          $customer_name = get_field('customer_name');
+          $customer_location = get_field('customer_city_state');
+          if($i === 1) {
+            echo '<div class="testimonial quote-'.$i.'">';
+              echo '<h3 class="text-center">'.$new_content.'</h3>';
+              echo '<h4 class="text-center">'.$customer_name.'</h4>';
+              echo '<h6 class="text-center">'.$customer_location.'</h6>';
+            echo '</div>';
+          } else {
+            echo '<div class="testimonial quote-'.$i.' hidden">';
+              echo '<h3 class="text-center">'.$new_content.'</h3>';
+              echo '<h4 class="text-center">'.$customer_name.'</h4>';
+              echo '<h6 class="text-center">'.$customer_location.'</h6>';
+            echo '</div>';
+          }
+          $i++;
+        }
+      }
+      ?>
     </div>
   </div>
   <div id="mission-statement" class="textured">
