d176bbb486cc69fd04b112da98208eab33b0d49c
athurman <athurman215@gmail.com>
2015-07-16 07:50:39 -0500

    add base what we offer page

---
+++ page-templates/page-whatweofferpage.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Template Name: What We Offer Page
+*/
+
+$main_header = get_field('main_what_we_offer_header');
+?>
+<?php get_header(); ?>
+<div class="content">
+  <!-- HERO SECTION -->
+  <div id="servicespage-hero">
+    <div class="wrap">
+      <h1 class="text-center main"><?php echo $main_header; ?></h1>
+    </div>
+  </div>
+  <div id="breakdown-services" class="section">
+    <div class="services wrap">
+      <div class="service textured">
+        <h1 class="text-center">Health</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="service green">
+        <h1 class="text-center">Fitness</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="service gray">
+        <h1 class="text-center">Wellness Retreats</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="service biege">
+        <h1 class="text-center">Corporate Wellness</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="service light-blue">
+        <h1 class="text-center">Nurtition</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="service textured">
+        <h1 class="text-center">Events</h1>
+        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
+        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
