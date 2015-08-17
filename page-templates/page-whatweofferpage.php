fb8fc2cc21cb942796edaffa2e3299865a1c80f5
athurman <athurman215@gmail.com>
2015-08-17 06:15:38 -0500

    update what we offer links and make text editable

--- page-templates/page-whatweofferpage.php
+++ page-templates/page-whatweofferpage.php
@@ -4,6 +4,12 @@ Template Name: What We Offer Page
 */
 
 $main_header = get_field('main_what_we_offer_header');
+$health_and_nutrition = get_field('health_and_nutrition');
+$fitness = get_field('fitness');
+$wellness_retreats = get_field('wellness_retreats');
+$corporate_wellness = get_field('corporate_wellness');
+$counseling = get_field('counseling');
+$events = get_field('events');
 ?>
 <?php get_header(); ?>
 <div class="content">
@@ -18,44 +24,56 @@ $main_header = get_field('main_what_we_offer_header');
       <div class="service textured">
         <h1 class="text-center">Health & Nutrition</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/nutrition-icon.png" alt="Nutrition Icon Plant">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+        <?php echo $health_and_nutrition; ?>
         <a href="#" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="#">Learn More</a>
+        </div>
       </div>
       <div class="service green">
         <h1 class="text-center">Fitness</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/fitness-icon.png" alt="Fitness Icon Yoga">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
-        <a href="#" class="gray-btn">Learn More</a>
+        <?php echo $fitness; ?>
+        <a href="<?php echo home_url(); ?>/fitness-services" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="<?php echo home_url(); ?>/fitness-services">Learn More</a>
+        </div>
       </div>
       <div class="service gray">
         <h1 class="text-center">Wellness Retreats</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/health-icon.png" alt="Health Icon Holding Plant">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+        <?php echo $wellness_retreats; ?>
         <a href="#" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="#">Learn More</a>
+        </div>
       </div>
       <div class="service biege">
         <h1 class="text-center">Corporate Wellness</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/merge-icon.png" alt="Merge Health, Fitness and Nutrition Icon">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+        <?php echo $corporate_wellness; ?>
         <a href="#" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="#">Learn More</a>
+        </div>
       </div>
       <div class="service light-blue">
         <h1 class="text-center">Counseling</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/counseling-icon.png" alt="Counseling Icon Word Bubbles">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+        <?php echo $counseling; ?>
         <a href="#" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="#">Learn More</a>
+        </div>
       </div>
       <div class="service textured">
         <h1 class="text-center">Events</h1>
         <img src="<?php echo get_template_directory_uri(); ?>/library/images/calendar-icon.png" alt="Calendar Icon">
-        <p>Cupcake ipsum dolor sit amet sugar plum cheesecake oat cake candy canes. Ice cream fruitcake marzipan. Jelly pastry soufflé cake tiramisu chocolate cake gingerbread.</p>
-        <p>Sugar plum bear claw candy canes wafer cake tart croissant candy canes lollipop. Jelly beans oat cake fruitcake muffin. Sweet toffee cookie chupa chups carrot cake apple pie chupa chups.</p>
+        <?php echo $events; ?>
         <a href="#" class="gray-btn">Learn More</a>
+        <div class="service-link">
+          <a href="#">Learn More</a>
+        </div>
       </div>
       <div class="clear"></div>
     </div>
