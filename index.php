17f918883417e756062c704b6407555f20b2c3a3
athurman <athurman215@gmail.com>
2015-08-20 07:31:32 -0500

    update size of content on post pages

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div id="content">
   <div id="inner-content" class="wrap cf">
-    <main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+    <main id="main" class="m-all t-2of3 d-3of4 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
           <header class="article-header">
--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 
 <div id="content">
   <div id="inner-content" class="wrap cf">
-    <main id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+    <main id="main" class="m-all t-2of3 d-3of4 cf" role="main">
       <h1 class="archive-title"><span><?php _e( 'Search Results for:', 'mergehealth' ); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
