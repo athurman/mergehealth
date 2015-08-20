8984714f69d094d1343afc1c5b0cff3afa8e971f
athurman <athurman215@gmail.com>
2015-08-19 21:58:46 -0500

    add contact page template

---
+++ page-templates/page-contact.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Contact Page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="content">
+  <!-- HERO SECTION -->
+  <div id="contactpage-hero">
+  </div>
+  <div id="contact-form" class="textured">
+    <div class="wrap">
+      <div class="form-container">
+        <form id="contact-us-form" method="post" action="">
+          <div class="t-all d-all  m-all">
+            <label for="first_name">Name*</label>
+            <input type="text" id="name" class="required" name="name" required autofocus />
+          </div>
+          <div class="t-all d-all  m-all">
+            <label for="email">Email*</label>
+            <input type="text" id="email" class="email required" name="email" required />
+          </div>
+          <div class="t-all d-all m-all">
+            <label for="phone">Phone Number</label>
+            <input type="text" id="phone" class="phone" name="phone" />
+          </div>
+          <div class="t-all d-all m-all">
+            <label for="message">Message*</label>
+            <textarea rows="10" name="message" id="message" class="required" required ></textarea>
+          </div>
+          <div class="clear"></div>
+          <input class="blue-btn t-all d-all m-all" type="submit" value="Submit" id="contact-form-submit">
+          <div class="clear"></div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
