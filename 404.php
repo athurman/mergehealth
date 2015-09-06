09cf9fa278ec80ede9a521fd98d73943c2cf6e95
athurman <athurman215@gmail.com>
2015-09-06 15:21:25 -0500

    update 404 page layout

--- 404.php
+++ 404.php
@@ -1,43 +1,8 @@
 <?php get_header(); ?>
-
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-						<article id="post-not-found" class="hentry cf">
-
-							<header class="article-header">
-
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'mergehealth' ); ?></h1>
-
-							</header>
-
-							<section class="entry-content">
-
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'mergehealth' ); ?></p>
-
-							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'mergehealth' ); ?></p>
-
-							</footer>
-
-						</article>
-
-					</main>
-
-				</div>
-
-			</div>
-
+  <div id="error-page-container">
+    <h1 class="main text-center">404</h1>
+    <h1 class="main text-center">It looks like you've lost your way.</h1>
+    <h1 class="sub-header text-center">Get back on track.</h1>
+    <a href="<?php echo home_url(); ?>" class="blue-btn">Home</a>
+  </div>
 <?php get_footer(); ?>
