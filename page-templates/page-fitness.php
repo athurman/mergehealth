7e617f98f3204cc78ccd0f7a24510e8917d4afb1
athurman <athurman215@gmail.com>
2015-09-06 14:01:45 -0500

    update 404 page design

--- page-templates/page-fitness.php
+++ page-templates/page-fitness.php
@@ -20,7 +20,7 @@ $swim_classes_text = get_field('swim_classes_text');
   <div id="personal-training" class="section">
     <div class="wrap">
       <h1 class="sub-main text-center">Personal Training</h1>
-      <a class="text-center" href="#">Learn More</a>
+      <a class="text-center" href="#" data-toggle="modal" data-target="#trainingInfoModal">Learn More</a>
       <div class="mobile-container">
         <?php
         if(!empty($personal_training_text)) {
@@ -33,27 +33,87 @@ $swim_classes_text = get_field('swim_classes_text');
   <div id="boot-camp" class="section">
     <div class="wrap">
       <h1 class="sub-main text-center">Boot Camp</h1>
-      <a class="text-center" href="#">Learn More</a>
+      <a class="text-center" href="#" data-toggle="modal" data-target="#bootcampInfoModal">Learn More</a>
       <div class="mobile-container">
         <?php
         if(!empty($boot_camp_text)) {
           echo $boot_camp_text;
         }
         ?>
-    </div>
+      </div>
     </div>
   </div>
   <div id="swim" class="section">
     <div class="wrap">
       <h1 class="sub-main text-center">Swim Classes</h1>
-      <a class="text-center" href="#">Learn More</a>
+      <a class="text-center" href="#" data-toggle="modal" data-target="#swimmingInfoModal">Learn More</a>
       <div class="mobile-container">
         <?php
         if(!empty($swim_classes_text)) {
           echo $swim_classes_text;
         }
         ?>
+      </div>
     </div>
+  </div>
+</div>
+<!-- Personal Training Modal -->
+<div class="modal fade" id="trainingInfoModal" tabindex="-1" role="dialog" aria-labelledby="fitnessInfo">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Personal Training</h4>
+      </div>
+      <div class="modal-body">
+        <?php
+        if(!empty($personal_training_text)) {
+          echo $personal_training_text;
+        }
+        ?>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Boot Camp Modal -->
+<div class="modal fade" id="bootcampInfoModal" tabindex="-1" role="dialog" aria-labelledby="fitnessInfo">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Boot Camp</h4>
+      </div>
+      <div class="modal-body">
+        <?php
+        if(!empty($boot_camp_text)) {
+          echo $boot_camp_text;
+        }
+        ?>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Swimming Modal -->
+<div class="modal fade" id="swimmingInfoModal" tabindex="-1" role="dialog" aria-labelledby="fitnessInfo">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Swim Classes</h4>
+      </div>
+      <div class="modal-body">
+        <?php
+        if(!empty($swim_classes_text)) {
+          echo $swim_classes_text;
+        }
+        ?>
+      </div>
+      <div class="modal-footer">
+      </div>
     </div>
   </div>
 </div>
