17df33caecfcecbdbc9f6456cce2fc26f3218142
athurman <athurman215@gmail.com>
2015-09-27 14:07:49 -0500

    update logic on events page

--- page-templates/page-events.php
+++ page-templates/page-events.php
@@ -15,45 +15,49 @@ $terms = get_terms( 'event_type', 'orderby=name&hide_empty=0' );
 <?php get_header(); ?>
 <div class="content">
   <!-- HERO SECTION -->
-  <div id="aboutpage-hero">
+  <div id="eventspage-hero">
     <div class="wrap">
-      <h1 class="text-center main"><?php echo $main_header; ?></h1>
+      <h1 class="text-center main">Events</h1>
     </div>
   </div>
   <!-- EVENTS -->
-  <?php
+  <div id="events-breakdown" class="section">
+    <div class="wrap">
+      <?php
 
-  foreach ( $terms as $term ) {
-    echo '<h2>'.$term->name.'</h2>';
-    $args=array(
-      'event_type' => $term->slug,
-      'post_type' => 'event',
-      'post_status' => 'publish',
-      'posts_per_page' => -1,
-    );
-    $events_query = new WP_Query($args);
-    $i = 0;
-    if( $events_query->have_posts() ) {
-      while ($events_query->have_posts()) : $events_query->the_post();
-      $event_date = get_field('event_date');
-      $event_time = strtotime($event_date);
-      $last_24_hours = time()-(60*60*24);
-      if($last_24_hours < $event_time) { ?>
-        <p><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-  <?php
-        $i++;
+      foreach ( $terms as $term ) {
+        echo '<h2 class="subheader">'.$term->name.'</h2>';
+        $args=array(
+          'event_type' => $term->slug,
+          'post_type' => 'event',
+          'post_status' => 'publish',
+          'posts_per_page' => -1,
+        );
+        $events_query = new WP_Query($args);
+        $i = 0;
+        if( $events_query->have_posts() ) {
+          while ($events_query->have_posts()) : $events_query->the_post();
+          $event_date = get_field('event_date');
+          $event_time = strtotime($event_date);
+          $last_24_hours = time()-(60*60*24);
+          if($last_24_hours < $event_time) { ?>
+            <p class="event"><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+      <?php
+            $i++;
+          }
+          endwhile;
+          if($i === 0) { ?>
+            <p class="no-events">No events scheduled at this time.</p>
+      <?php
+          }
+        } else { ?>
+        <p class="no-events">No events scheduled at this time.</p>
+      <?php
+        }
+        wp_reset_query();
       }
-      endwhile;
-      if($i === 0) { ?>
-        <p>No events scheduled at this time.</p>
-    <?php
-      }
-    } else { ?>
-    <p>No events scheduled at this time.</p>
-  <?php
-    }
-    wp_reset_query();
-  }
-  ?>
+      ?>
+    </div>
+  </div>
 </div>
 <?php get_footer(); ?>
